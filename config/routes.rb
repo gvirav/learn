a75a7173ee376bd5a92e97a0e78f63f9e6b95d5a
Michael Wiggins <wigginsmichaelc@gmail.com>
2013-02-10 01:01:18 -0800

    Add named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Learn::Application.routes.draw do
   devise_for :users
 
+  as :user do
+    get '/register', to: 'devise/registrations#new', as: :register
+    get '/account', to: 'devise/registrations#edit', as: :account
+    get '/login', to: 'devise/sessions#new', as: :login
+    get '/logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
+  as :user do
+    get '/login' => 'devise/sessions#new', as: :new_user_session
+    post '/login' => 'devise/sessions#create', as: :user_session
+    delete '/logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   resources :notes, only: :index
   
   resources :checkpoints do 
